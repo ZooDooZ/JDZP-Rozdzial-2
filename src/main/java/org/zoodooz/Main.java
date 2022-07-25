2a61acf6e7d412a5d4b8830acb9629a27efcc633
Filip Sokolowski <filipsokolowskiah@gmail.com>
2022-07-25 08:34:29 +0200

    Rysunek z gwiazdek

--- src/main/java/org/zoodooz/Main.java
+++ src/main/java/org/zoodooz/Main.java
@@ -2,6 +2,13 @@ package org.zoodooz;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        
+        System.out.println("    *     ");
+        System.out.println("   ***    ");
+        System.out.println("  *****   ");
+        System.out.println(" *******  ");
+        System.out.println("  *****   ");
+        System.out.println("   ***    ");
+        System.out.println("    *     ");
     }
 }
