8b9d28f51856da6ebf95efae922fd5e59a309f5b
Filip Sokolowski <filipsokolowskiah@gmail.com>
2022-07-25 08:36:11 +0200

    Prognozy sprzedaży

--- src/main/java/org/zoodooz/Main.java
+++ src/main/java/org/zoodooz/Main.java
@@ -2,6 +2,11 @@ package org.zoodooz;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        double sellValuePerYear = 4600000.0;
+        double sellValueMasovianRegion;
+
+        sellValueMasovianRegion = sellValuePerYear * 0.62;
+        System.out.println("W ciągu roku region mazowiecki wygeneruje sprzedaż: " + sellValueMasovianRegion + " zł");
     }
 }
