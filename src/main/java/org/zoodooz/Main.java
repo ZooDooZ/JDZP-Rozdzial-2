e53057430c49a6245742f9d2cfa1ecdb39587652
Filip Sokolowski <filipsokolowskiah@gmail.com>
2022-07-25 08:27:54 +0200

    Nazwisko, wiek i roczny dochód

--- src/main/java/org/zoodooz/Main.java
+++ src/main/java/org/zoodooz/Main.java
@@ -2,6 +2,16 @@ package org.zoodooz;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        String name;
+        int age;
+        double annualPlay;
+
+        name = "Filip Sokołowski";
+        age = 32;
+        annualPlay =  80000.0;
+
+        System.out.println("Nazywam się " + name + ", mam " + age + " lat\ni chcę zarabiać " +
+                            annualPlay + " złotych rocznie.");
     }
 }
