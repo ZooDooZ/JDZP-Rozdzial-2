42df25cab000dc920a8e19a7beb8ed72d7d67dba
Filip Sokolowski <filipsokolowskiah@gmail.com>
2022-07-25 08:31:22 +0200

    Dane osobowe

--- src/main/java/org/zoodooz/Main.java
+++ src/main/java/org/zoodooz/Main.java
@@ -2,6 +2,15 @@ package org.zoodooz;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        
+        String nameAndSurname = "Filip Sokołowski";
+        String address = "Ponurego 28 B";
+        String phoneNumber = "791-450-432";
+        String nameOfStudy = "Programowanie";
+
+        System.out.println("Imię i nazwisko: " + nameAndSurname + "\n" +
+                            "Adres zamieszkania: " + address + "\n" +
+                            "Numer telefonu: " + phoneNumber + "\n" +
+                            "Kierunek studiów: " + nameOfStudy);
     }
 }
