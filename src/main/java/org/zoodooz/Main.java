3853ab7eda9cf25b3afdc06ed2a16a85f4c795cd
Filip Sokolowski <filipsokolowskiah@gmail.com>
2022-07-25 08:37:16 +0200

    Obliczenia powierzchni

--- src/main/java/org/zoodooz/Main.java
+++ src/main/java/org/zoodooz/Main.java
@@ -2,6 +2,12 @@ package org.zoodooz;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        
+        double oneAcre = 43560.0;
+        double wholeArea = 389767.0;
+        double result;
+
+        result = wholeArea / oneAcre;
+        System.out.println("Na obszarze zajmującym " + wholeArea + " jest: " + result + " akrów.");
     }
 }
