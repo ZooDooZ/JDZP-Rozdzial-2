8064bb10f90ef20b3ae5f5bdb25ecfce457c0ea2
Filip Sokolowski <filipsokolowskiah@gmail.com>
2022-07-25 08:43:39 +0200

    Amerykański podatek od sprzedaży

--- src/main/java/org/zoodooz/Main.java
+++ src/main/java/org/zoodooz/Main.java
@@ -1,7 +1,24 @@
 package org.zoodooz;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        
+        Scanner keyborad = new Scanner(System.in);
+
+        System.out.println("Wprowadź wartość wprowadzanego produktu: ");
+        double valueOfProduct = keyborad.nextDouble();
+        System.out.println("Wartość produktu wynosi: " + valueOfProduct + " USD");
+
+        double stateTax = valueOfProduct * 0.04;
+        System.out.println("Podatek stanowy od tego produktu wynosi: " + stateTax + " USD");
+
+
+        double localTax = valueOfProduct * 0.02;
+        System.out.println("Podatek lokalny od tego produktu wynosi: " + localTax + " USD");
+
+        double finalPriceOfProduct = valueOfProduct + stateTax + localTax;
+        System.out.println("Lączna cena sprzedaży tego produktu wynosi: " + finalPriceOfProduct + " USD");
     }
 }
