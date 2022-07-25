2bc88bbe7c8eb68325630abb6e187287c030f0dc
Filip Sokolowski <filipsokolowskiah@gmail.com>
2022-07-25 08:46:12 +0200

    Kilometry na litrze

--- src/main/java/org/zoodooz/Main.java
+++ src/main/java/org/zoodooz/Main.java
@@ -1,7 +1,23 @@
 package org.zoodooz;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        
+        float kmPerLiter;
+        double kilometersTraveled;
+        double petrolLiter;
+
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.println("Wprowadź liczbę przejechanych kilometrów: ");
+        kilometersTraveled = keyboard.nextDouble();
+
+        System.out.println("Podaj liczbę zużytych litrów paliwa: ");
+        petrolLiter =  keyboard.nextDouble();
+
+        kmPerLiter = (float) kilometersTraveled / (float) petrolLiter;
+        System.out.println("Na jednym litrze przejechałeś: " + kmPerLiter + " km.");
     }
 }
