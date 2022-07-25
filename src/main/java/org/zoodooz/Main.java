d055b6a02753829ead5b0e3c4756581b98d7913e
Filip Sokolowski <filipsokolowskiah@gmail.com>
2022-07-25 08:55:18 +0200

    Spożycie napojów energetycznych

--- src/main/java/org/zoodooz/Main.java
+++ src/main/java/org/zoodooz/Main.java
@@ -2,6 +2,15 @@ package org.zoodooz;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        
+        int numberOfConsumers = 12467;
+
+        int consumersBuyEnergeticPerWeek = (int) (numberOfConsumers * 0.14);
+        int consumersBuyCitrusEnergetic = (int) (consumersBuyEnergeticPerWeek * 0.64);
+
+        System.out.println("Przybliżona liczba ankietowanych osób kupująca przynajmniej \n" +
+                           "jeden napój energetyczny tygodniowo: " + consumersBuyEnergeticPerWeek);
+        System.out.println("Przybliżona liczba ankietowanych osób preferująca napoje energetyczne \n" +
+                            "o smaku cytrusowym: " + consumersBuyCitrusEnergetic);
     }
 }
