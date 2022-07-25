6d931bf19f250d18b95548a66a6a011bf0e15aaf
Filip Sokolowski <filipsokolowskiah@gmail.com>
2022-07-25 08:47:34 +0200

    Średnia z testów

--- src/main/java/org/zoodooz/Main.java
+++ src/main/java/org/zoodooz/Main.java
@@ -1,7 +1,28 @@
 package org.zoodooz;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        
+        Scanner keyboard = new Scanner(System.in);
+
+        double avarageOfTest;
+
+        System.out.println("Podaj swój wynik z pierwszego testu: ");
+        double firstTest = keyboard.nextDouble();
+
+        System.out.println("Podaj swój wynik z drugiego testu: ");
+        double secondTest = keyboard.nextDouble();
+
+        System.out.println("Podaj swój wynik z trzeciego testu: ");
+        double thirdTest = keyboard.nextDouble();
+
+        System.out.println("Wynik z pierwszego testu: " + firstTest);
+        System.out.println("Wynik z drugiego testu: " + secondTest);
+        System.out.println("Wynik z trzeciego testu: " + thirdTest);
+
+        avarageOfTest = (firstTest + secondTest + thirdTest) / 3;
+        System.out.println("Twój średni wynik z trzech testów: " + avarageOfTest);
     }
 }
