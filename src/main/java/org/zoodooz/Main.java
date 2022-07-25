d71660b14e1e9431b77656da9029f6701d59dad0
Filip Sokolowski <filipsokolowskiah@gmail.com>
2022-07-25 08:50:31 +0200

    Operacje na łańcuchach znaków

--- src/main/java/org/zoodooz/Main.java
+++ src/main/java/org/zoodooz/Main.java
@@ -1,7 +1,17 @@
 package org.zoodooz;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Podaj nazwę twojego ulubionego miasta: ");
+        String townName = keyboard.nextLine();
+
+        System.out.println("Liczba znaków w nazwie miasta: "+ townName.length());
+        System.out.println("Nazwa miasta z wielkiej litery: "+ townName.toUpperCase());
+        System.out.println("Nazwa miasta z małej litery: "+ townName.toLowerCase());
+        System.out.println("Pierwsza litera miasta: "+ townName.charAt(0));
     }
 }
