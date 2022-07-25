35bfdb4546115fc8a53dfc0ca0bb5dae679b421d
Filip Sokolowski <filipsokolowskiah@gmail.com>
2022-07-25 09:00:42 +0200

    Sadzenie winogron

--- src/main/java/org/zoodooz/Main.java
+++ src/main/java/org/zoodooz/Main.java
@@ -1,7 +1,33 @@
 package org.zoodooz;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        //długość rzędu w metrach
+        double rowLength;
+        //ilość miejsca zajmowanego przez okratowania w metrach
+        double trellisArea;
+        //odległość miedzy sadzonkami w metrach
+        double lengthBetweenSeedling;
+        //liczba sadzonek jakie można uwmieścić w rzędzie
+        double numberOfSeeds;
+
+        Scanner keyboard =  new Scanner(System.in);
+
+        System.out.println("Wprowadź długość rzędu w metrach: ");
+        rowLength = keyboard.nextDouble();
+
+        System.out.println("Wprowadź ilość miejsca zajmowanego przez okratowanie w metrach: ");
+        trellisArea = keyboard.nextDouble();
+
+        System.out.println("Wprowadź odległość między sadzonkami w metrach: ");
+        lengthBetweenSeedling = keyboard.nextDouble();
+
+        numberOfSeeds = (rowLength - 2 * trellisArea) / lengthBetweenSeedling;
+        int numberOfSeedsInt = (int)numberOfSeeds;
+
+        System.out.println("W jednym rzędzie zmieścisz: " + numberOfSeedsInt + " sadzonek.");
     }
 }
