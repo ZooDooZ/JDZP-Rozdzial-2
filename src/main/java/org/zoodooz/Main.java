3d47832c9d1f685ad6bbed4ee7d74c83cafca8da
Filip Sokolowski <filipsokolowskiah@gmail.com>
2022-07-25 08:45:03 +0200

    Kalorie w ciasteczkach

--- src/main/java/org/zoodooz/Main.java
+++ src/main/java/org/zoodooz/Main.java
@@ -1,7 +1,21 @@
 package org.zoodooz;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        
+        int oneCookieKcal = 75;
+        int howManyCookies;
+        int howManyKcalEat;
+
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.println("Wpisz ile ciasteczek zjadłeś/aś a podam Ci ile spożyłeś/aś kalorii: ");
+        howManyCookies = keyboard.nextInt();
+        System.out.println("Zjadłeś/aś " + howManyCookies + " ciasteczek.");
+
+        howManyKcalEat = howManyCookies * oneCookieKcal;
+        System.out.println("To oznacza, że spożyłeś/aś " + howManyKcalEat + " kcal.");
     }
 }
