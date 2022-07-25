39c085586770ff46e84bab1968e655463a68518a
Filip Sokolowski <filipsokolowskiah@gmail.com>
2022-07-25 08:51:51 +0200

    Rachunek w restauracji

--- src/main/java/org/zoodooz/Main.java
+++ src/main/java/org/zoodooz/Main.java
@@ -1,7 +1,21 @@
 package org.zoodooz;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.println("Wprowadź cenę posiłku: ");
+        double mealPrice = keyboard.nextDouble();
+        double taxOfMeal = mealPrice * 0.0675;
+        double tipForMeal = (mealPrice + taxOfMeal) * 0.2;
+        double totalPrice = mealPrice + taxOfMeal + tipForMeal;
+
+        System.out.println("Cena posiłku: " + mealPrice);
+        System.out.println("Wartość podatku: " + taxOfMeal);
+        System.out.println("Wysokość napiwku: " + tipForMeal);
+        System.out.println("Lączna kwota do zapłaty: " + totalPrice);
     }
 }
