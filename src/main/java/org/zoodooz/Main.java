5451d2b1475d777bf20f7bc3a684eec65c0d8cc5
Filip Sokolowski <filipsokolowskiah@gmail.com>
2022-07-25 08:54:07 +0200

    Prowizje od handlu akcjami

--- src/main/java/org/zoodooz/Main.java
+++ src/main/java/org/zoodooz/Main.java
@@ -2,6 +2,20 @@ package org.zoodooz;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int stocks =  600;
+        double priceOneStock = 21.77;
+        double priceOfAllStocks;
+        double brokerCommission;
+        double totalPriceForStocks;
+
+        priceOfAllStocks = stocks * priceOneStock;
+        brokerCommission = priceOfAllStocks * 0.02;
+        totalPriceForStocks = priceOfAllStocks + brokerCommission;
+
+
+        System.out.println("Katarzyna zapłaciła za akcje: " + priceOfAllStocks + " zł.");
+        System.out.println("Prowizja brokera wynosi: " + brokerCommission + " zł");
+        System.out.println("Katarzyna w sumie zapłaciła: " + totalPriceForStocks + " zł, za " + stocks + " akcji.");
     }
 }
