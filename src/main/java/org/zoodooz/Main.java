affe26338a75fc8e05a95e8a200aec2b9d4a9421
Filip Sokolowski <filipsokolowskiah@gmail.com>
2022-07-25 08:52:52 +0200

    Odsetek kobiet i mężczyzn

--- src/main/java/org/zoodooz/Main.java
+++ src/main/java/org/zoodooz/Main.java
@@ -1,7 +1,30 @@
 package org.zoodooz;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        double numberOfWomen;
+        double numberOfMan;
+        double allPeople;
+
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.println("Wprowadź liczbę kobiet zarejestrowanych na kurs: ");
+        numberOfWomen = keyboard.nextInt();
+
+        System.out.println("Wprowadź liczbę mężczyzn zarejestrowanych na kurs: ");
+        numberOfMan = keyboard.nextInt();
+
+        allPeople = numberOfMan + numberOfWomen;
+        float percentageOfWomen = (float)numberOfWomen / (float)allPeople;
+        float percentageOfMan = (float)numberOfMan / (float)allPeople;
+
+        percentageOfWomen = percentageOfWomen * 100;
+        percentageOfMan = percentageOfMan * 100;
+
+        System.out.println("Odsetek kobiet na kursie: " + percentageOfWomen + " %\n" +
+                            "Odsetek mężczyzn na kursie: " + percentageOfMan + " %");
     }
 }
