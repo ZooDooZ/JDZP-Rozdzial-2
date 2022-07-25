0a5d49bd81190c8e6a4752e57e9725eb25c22127
Filip Sokolowski <filipsokolowskiah@gmail.com>
2022-07-25 08:49:19 +0200

    Zyski ze sprzedaży płytek drukowanych

--- src/main/java/org/zoodooz/Main.java
+++ src/main/java/org/zoodooz/Main.java
@@ -1,7 +1,16 @@
 package org.zoodooz;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.println("Wprowadź cenę detaliczną płytki: ");
+        double tileRetailPrice = keyboard.nextDouble();
+        double profit = tileRetailPrice * 0.4;
+
+        System.out.println("Twój zysk z płytki: " + profit + " zł.");
     }
 }
